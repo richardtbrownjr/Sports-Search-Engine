c7f1904d6828498e7dd7a24ae93f293ef1b7a3ad
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2016-08-10 07:46:41 -0400

    search working

--- sports_search.py
+++ sports_search.py
@@ -2,6 +2,18 @@ import os
 import psycopg2
 
 
+def get_input():
+    os.system('clear')
+    print("Would you like to: ")
+    print("1.) Search current Panthers players")
+    print("2.) Add new player")
+    key_stroke = input("Type 1 or 2 for your choice  ")
+    if key_stroke in ['1', '2']:
+        return int(key_stroke)
+    else:
+        get_input
+
+
 def add_new_player():
     os.system('clear')
     con = psycopg2.connect("dbname=panthers2 user=richardt.brownjr.")
@@ -13,7 +25,7 @@ def add_new_player():
     new_rushing = input("Yards Rushing: ")
     new_receiving = input("Yards Receving: ")
     td = input("number of touch downs ")
-    cur.execute("""INSERT INTO panthers2 (Num, Name, Pos, Age, YRushing, YReceiving,TD) VALUES (%s, %s, %s, %s, %s, %s, %s)""", (name, numb, position, new_age, new_rushing, new_receiving, td))
+    cur.execute("""INSERT INTO panthers2 (Num, Name, Age, Pos, YRushing, YReceiving,TD) VALUES (%s, %s, %s, %s, %s, %s, %s)""", (name, numb, position, new_age, new_rushing, new_receiving, td))
     con.commit()
     cur.close()
     con.close()
@@ -24,20 +36,22 @@ def search_players():
     con = psycopg2.connect("dbname=panthers2 user=richardt.brownjr.")
     cur = con.cursor()
     pname = input("Name of Panther player you want to find in database? ")
-    for row in cur:
-        if pname == name:
-            print(row)
+    cur.execute("SELECT * FROM panthers2 WHERE Name=%(name)s", {'name': pname})
+    print(" Num,     Name,       Age,  Pos,   YRushing,  YReceiving,  TD")
+    print (cur.fetchone())
     con.commit()
     cur.close()
     con.close()
 
 
-
 def main():
     # con = psycopg2.connect("dbname=panthers2 user=richardt.brownjr.")
     # cur = con.cursor()
-    #add_new_player()
-    search_players()
+    key_stroke = get_input()
+    if key_stroke == 2:
+        add_new_player()
+    if key_stroke == 1:
+        search_players()
     # con.commit()
     # cur.close()
     # con.close()
